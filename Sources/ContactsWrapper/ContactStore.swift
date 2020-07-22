c7c5ba4befdc31afe9a079617d26115ae17851b6
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2020-07-22 21:47:51 +0200

    Added ContactStore add, update, delete methods

--- Sources/ContactsWrapper/ContactStore.swift
+++ Sources/ContactsWrapper/ContactStore.swift
@@ -78,3 +78,99 @@ extension CNContactStore {
         }.eraseToAnyPublisher()
     }
 }
+
+extension CNContactStore {
+    public typealias SaveRequestPublisher = AnyPublisher<Bool, Error>
+    
+    private func execute(request: CNSaveRequest) -> SaveRequestPublisher {
+        return Future { completion in
+            self.requestAccess(for: .contacts) { (authorized, error) in
+                do {
+                    guard authorized else {
+                        throw error ?? CNError(.authorizationDenied)
+                    }
+
+                    try self.execute(request)
+                    
+                    completion(
+                        .success(true)
+                    )
+                } catch {
+                    completion(
+                        .failure(error)
+                    )
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    // MARK: Save
+    
+    public func add(_ contact: CNMutableContact, toContainerWithIdentifier containerIdentifier: String? = nil) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.add(contact, toContainerWithIdentifier: containerIdentifier)
+        return execute(request: saveRequest)
+    }
+    
+    public func save(_ group: CNMutableGroup, toContainerWithIdentifier containerIdentifier: String? = nil) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.add(group, toContainerWithIdentifier: containerIdentifier)
+        return execute(request: saveRequest)
+    }
+    
+    // MARK: Add member
+    
+    public func addMember(_ contact: CNContact, to group: CNGroup) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.addMember(contact, to: group)
+        return execute(request: saveRequest)
+    }
+    
+    public func addSubgroup(_ subgroup: CNGroup, to group: CNGroup) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.addSubgroup(subgroup, to: group)
+        return execute(request: saveRequest)
+    }
+    
+    // MARK: Update
+    
+    public func update(_ contact: CNMutableContact) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.update(contact)
+        return execute(request: saveRequest)
+    }
+    
+    public func update(_ group: CNMutableGroup) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.update(group)
+        return execute(request: saveRequest)
+    }
+    
+    // MARK: Delete
+    
+    public func delete(_ contact: CNMutableContact) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.delete(contact)
+        return execute(request: saveRequest)
+    }
+    
+    public func delete(_ group: CNMutableGroup) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.delete(group)
+        return execute(request: saveRequest)
+    }
+    
+    // MARK: Remove member
+    
+    public func removeMember(_ contact: CNContact, to group: CNGroup) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.removeMember(contact, from: group)
+        return execute(request: saveRequest)
+    }
+    
+    public func removeSubgroup(_ subgroup: CNGroup, to group: CNGroup) -> SaveRequestPublisher {
+        let saveRequest = CNSaveRequest()
+        saveRequest.removeSubgroup(subgroup, from: group)
+        return execute(request: saveRequest)
+    }
+}
